9330ff589fb7820c0618b8b6d651afce9a2ac1a9
RoyRoll <148009669+RoyRoll@users.noreply.github.com>
2023-10-16 02:50:17 +0700

    Add files via upload

---
+++ looping.py
@@ -0,0 +1,8 @@
+# x:['1','2','3']
+# for bilangan in x:
+#     print('Salah satu bilangan adalah', bilangan)
+
+days_of_week = ["Senin", "Selasa", "Rabu", "Kamis", "Jum'at", "Sabtu", "Minggu"]
+
+for day in days_of_week:
+    print(day)
